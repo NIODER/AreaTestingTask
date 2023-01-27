a677e3b7887f8792fbbd1def7485cfdf6629161b
Александр Кожаринов <nioder125@gmail.com>
2023-01-27 21:30:05 +0300

    Добавил sql запрос для выборки.

---
+++ SQL/sql_query.sql
@@ -0,0 +1,8 @@
+-- Пусть таблица категорий называется categories, а таблица продуктов products.
+-- А промежуточная таблица - catprod
+-- Идентификаторы хранятся в атрибутах с именем "id".
+SELECT (products.name, categories.name) FROM products
+INNER JOIN catprod
+ON products.id = catprod.product_id
+LEFT JOIN categories
+ON catprod.category_id = categories.id
